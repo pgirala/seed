e0c2f20dcf5a86d824eb703aa9dafeb844fe50bd
pgirala <pgirala@gmail.com>
2020-12-12 00:32:22 +0100

    first commit

---
+++ src/test/java/oipdii/comun/seed/SeedApplicationTests.java
@@ -0,0 +1,13 @@
+package oipdii.comun.seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
