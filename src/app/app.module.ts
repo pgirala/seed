c661e7a76910296d4a1527922096bf4fd501d367
pgirala <pgirala@gmail.com>
2020-12-15 19:53:42 +0100

    Identificación con login terminada (con fallos)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,21 @@ import { AppService } from './app.service';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login.component';
 import { AppComponent } from './app.component';
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
+} from '@angular/common/http';
 
+@Injectable()
+export class XhrInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const xhr = req.clone({
+      headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
+    });
+    return next.handle(xhr);
+  }
+}
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent},
@@ -26,7 +40,7 @@ const routes: Routes = [
     HttpClientModule,
     FormsModule
   ],
-  providers: [AppService],
+  providers: [AppService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main/java/oip/dii/comun/seed/SeedApplication.java
+++ src/main/java/oip/dii/comun/seed/SeedApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 @RestController
@@ -21,11 +22,13 @@ public class SeedApplication {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
       http
-        .httpBasic()
-      .and()
+        .httpBasic().and()
         .authorizeRequests()
           .antMatchers("/index.html", "/", "/home", "/login").permitAll()
-          .anyRequest().authenticated();
+          .anyRequest().authenticated()
+          .and()
+        .csrf()
+          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
   }
 
